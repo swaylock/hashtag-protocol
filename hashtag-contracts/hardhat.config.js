67e156db588c649e25c8f7446ccd2d8d2020eb17
swaylock <admin@swaylocks.com>
2021-07-02 10:33:42 -0400

    Add ABI support and ethernal support to hardhat.
    gh-216

--- hashtag-contracts/hardhat.config.js
+++ hashtag-contracts/hardhat.config.js
@@ -3,27 +3,77 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-solhint");
+require('hardhat-abi-exporter');
+require('hardhat-ethernal');
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
-const MNEMONIC = process.env.PROTOTYPE_BR_KEY || "";
-const INFURA_KEY = process.env.PROTOTYPE_BR_INFURA_KEY || "";
-const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY || "";
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const INFURA_PROJECT_ID = process.env.PROTOTYPE_BR_INFURA_KEY || '';
+const PRIVATE_KEY = process.env.HASHTAG_PRIVATE_KEY || '0000000000000000000000000000000000000000000000000000000000000000';
+const MATICVIGIL_PROJECT_ID = process.env.MATICVIGIL_PROJECT_ID || '';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  compilers: {
-    solc: {
-      version: "0.6.12",
-      settings: {
-        optimizer: {
-          enabled: true, // Default: false
-          runs: 200, // Default: 200
-        },
-      },
+  solidity: {
+    version: '0.6.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: false
+  },
+  networks: {
+    hardhat: {
+      gasPrice: 00000000000, // 0 gwei
     },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`${PRIVATE_KEY}`],
+      gasPrice: 140000000000 // 140 gwei
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${MATICVIGIL_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      gasPrice: 2000000000, // 2 gwei
+    },
+    coverage: {
+      url: 'http://localhost:8555',
+      gasPrice: 8000000000, // 8 gwei
+    }
   },
-  networks: {},
-  api_keys: {
-    etherscan: ETHERSCAN_KEY,
-  },
+  abiExporter: {
+    path: './artifacts/abi',
+  }
 };
