0c047051901bd91b324da291519bd54ba73b0042
swaylock <admin@swaylocks.com>
2021-08-28 08:46:09 -0700

    Line lengths.
    gh-216

--- hashtag-contracts/deploy/02_hashtag_protocol.js
+++ hashtag-contracts/deploy/02_hashtag_protocol.js
@@ -2,18 +2,11 @@ const { ethers } = require("hardhat");
 
 module.exports = async ({ deployments }) => {
   const { deploy } = deployments;
-  const accountHashtagAdmin = await ethers.getNamedSigner(
-    "accountHashtagAdmin"
-  );
-  const accountHashtagPlatform = await ethers.getNamedSigner(
-    "accountHashtagPlatform"
-  );
+  const accountHashtagAdmin = await ethers.getNamedSigner("accountHashtagAdmin");
+  const accountHashtagPlatform = await ethers.getNamedSigner("accountHashtagPlatform");
 
   // Fetch address of HashtagAccessControls.
-  const contractAccessControls = await ethers.getContract(
-    "HashtagAccessControls",
-    accountHashtagAdmin
-  );
+  const contractAccessControls = await ethers.getContract("HashtagAccessControls", accountHashtagAdmin);
 
   await deploy("HashtagProtocol", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
