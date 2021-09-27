296d5b5150dbacc08f768ef8d540f28a55277d5c
swaylock <admin@swaylocks.com>
2021-09-27 14:16:01 -0700

    Solidiy linting and formatting.
    gh253

---
+++ hashtag-contracts/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  overrides: [
+    {
+      files: '*.sol',
+      options: {
+        printWidth: 120,
+        tabWidth: 4,
+        singleQuote: false,
+        explicitTypes: 'always',
+      },
+    },
+  ],
+};
